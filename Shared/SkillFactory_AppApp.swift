0c6b5e95c6b396e5df7adf4020ac69d055958b24
Владислав Прусаков <v.prusakov@corp.mail.ru>
2021-02-05 03:07:05 +0300

    Initial Commit

---
+++ Shared/SkillFactory_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillFactory_AppApp.swift
+//  Shared
+//
+//  Created by v.prusakov on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct SkillFactory_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
